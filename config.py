33b6a988165983dc99739b7eb031398471569d4f
Boran-Antunna <yakasirivamsivamsivamsi7@gmail.com>
2024-06-08 14:51:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,19 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6513299149:AAGVFtvdp6HUP50DtR0MALdQCFnbDYYje3U")
+API_ID = int(os.environ.get("API_ID", "25976580"))
+API_HASH = os.environ.get("API_HASH", "b5562ab77a96e49bc9dd78cc103c6333")
+OWNER = int(os.environ.get("OWNER", "6708929009"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "FileReNamerrBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "MOVIEZ_CARTOONZ")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002092836644")
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
-
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
-
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://developervro:developerrapuka@cluster9.gcj9754.mongodb.net/?retryWrites=true&w=majority&appName=Cluster9")
+DB_NAME = os.environ.get("DB_NAME", "developervro")
+STRING = os.environ.get("STRING", "BQGMXwQAi4Lh-ZTuxKxgTS6lo1izQUmpkugRRfYj6uul6AkVKn9k7TmuJCxr6fDdSPNYngpWP3wpU2FgVHWiMV5YJlEQHbsuslIWDi95MvCWCGoeXyVyTQLTbgqeDl10fvNvfDocZ07dxYDYyFqwicsCKQRZnwtyzw1kXxZHfCXO6idMNXMAiIrNBLhMKTIBpGhwFpP52qqqRRXFf_Der-4GDMv7Wy7rUQOaW_sPtgNU5YqilVrWDXGeo8PKdqPKJUusfMKIagTF9ZHWl0to0F9BefeSwCoRx2asQLXIxX0oYYl6KXuSkKPYWk2DW8yWD26xNgG2K7tImX6LjRq-33535cOFewAAAAGP4iHxAA")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/2a9c19d8cb0c82d0908f4.jpg")
 
 
 
